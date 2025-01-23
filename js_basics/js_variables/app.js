af35d7afe310851f1d1243773ac08afbf517581d
Taranpreet Kaur <kaurtaran367@gmail.com>
2025-01-23 14:31:25 +0530

    variables

---
+++ js_basics/js_variables/app.js
@@ -0,0 +1,22 @@
+let username = 'Taran';
+username = 'Aman';
+console.log(username);
+
+const PI = 3.14; //const value remains same 
+// PI=4; it will throw an error if we pass another value to the const PI
+
+let name = 'merry';
+ age = 28;
+ gender = 'female';
+
+ let country = 'USA'
+ let coun = 'UK';
+ country=coun;
+ console.log(country)
+
+ //RULES
+ // Variables name contains only letter,digits,underscores and dollar sign
+ //the first character of variable must not be digit
+ //variable name starts with underscore(_), or dollar sign($)
+ //js variables are case senstive
+ //reserved words cannot used as variable name 
