cc5c71525b76d24f8341efde806b26e216b16abb
Taranpreet Kaur <kaurtaran367@gmail.com>
2025-01-24 22:20:33 +0530

    commit

--- js_basics/js_operators/script.js
+++ js_basics/js_operators/script.js
@@ -6,6 +6,26 @@
 
 let x = 5;
 x = -x; //unary
-console.log(x)
+//console.log(x)
 
-let b = 30*2; //binary
+let y = 30*2; //binary 
+
+//ARITHEMATIC OPERATOR
+let a = 10;
+let b = 2;
+console.log(`Addition of ${a} and ${b} is ${a+b}`)
+console.log(`Subtraction of ${a} and ${b} is ${a-b}`)
+console.log(`Multiplication of ${a} and ${b} is ${a*b}`)
+console.log(`Division of ${a} and ${b} is ${a/b}`)
+console.log(`Modulus of ${a} and ${b} is ${a%b}`)
+console.log(`Exponent of ${a} and ${b} is ${a**b}`)
+
+//ASSIGNMENT OPERATOR
+let c = 5; // Assigns 5 to c
+c+=3; // Same as x = x + 3
+x -= 2; // Same as x = x - 2
+x *= 2; // Same as x = x * 2
+x /= 2; // Same as x = x / 2
+x %= 3; // Same as x = x % 3
+x **= 3; // Same as x = x ** 3
+console.log(c);
