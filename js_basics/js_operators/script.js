8ae96898711444fe40d48fe54894f6b2066e9f60
Taranpreet Kaur <kaurtaran367@gmail.com>
2025-01-24 20:30:25 +0530

    operators

---
+++ js_basics/js_operators/script.js
@@ -0,0 +1,11 @@
+//Operator performs some operations on a single or multiple operand and produce results.
+//Operator:An operand is what operators are applied to
+//1. Unary Operator
+//2. Binary Operator
+//3. Ternary Operator
+
+let x = 5;
+x = -x; //unary
+console.log(x)
+
+let b = 30*2; //binary
