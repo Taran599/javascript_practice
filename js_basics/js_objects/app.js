f9560759d3529cba8719be07657c1e762db28d76
Taranpreet Kaur <kaurtaran367@gmail.com>
2025-01-29 20:45:48 +0530

    commit

--- js_basics/js_objects/app.js
+++ js_basics/js_objects/app.js
@@ -1,3 +1,14 @@
-let user =  new Object();
-user.name = 'taran';
-user.age = 28;
+//let user = (); ()-> new object()
+//let user =  new Object();
+let user = {
+    name :'taran',
+    age: 28
+};
+console.log(user);
+console.log(user.name);
+console.log(user.age);
+
+user.gender = 'male'
+user['gender'] = 'female'
+delete user.age
+console.log(user);
