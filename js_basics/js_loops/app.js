933aaa717e035a74cb79fc9a76b825af49499295
Taranpreet Kaur <kaurtaran367@gmail.com>
2025-01-30 21:10:03 +0530

    commit

---
+++ js_basics/js_loops/app.js
@@ -0,0 +1,16 @@
+//STEP1: Create a variable and initialize it.
+//STEP2: Provide a condition for while loop.
+//STEP3: Increment/Decrement variable value.
+
+let i = 10;
+const user = ['steve','mark','ella','bella']
+
+// while(i>=1){
+// console.log("value of i is:" + i--)
+// }
+
+let index = 0;
+while(index<user.length){
+console.log(user[index]);
+index++;
+}
