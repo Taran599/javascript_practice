fbda2cd8580aa1e847de9d6d37d9ab81687ad3a9
Taranpreet Kaur <kaurtaran367@gmail.com>
2025-01-30 22:48:50 +0530

    commit

--- js_basics/js_loops/app2.js
+++ js_basics/js_loops/app2.js
@@ -1,7 +1,7 @@
 //do-while loop
 
 let i = 1;
-const user = ['steve','ella','mark','bella'];
+const user = ["steve", "ella", "mark", "bella"];
 //STEP1: Create a variable and initialize it.
 //STEP2: Increment/Decrement variable value.
 //STEP3: Provide a condition for while loop.
@@ -12,7 +12,20 @@ const user = ['steve','ella','mark','bella'];
 // while(i<=10)
 
 let index = 0;
-do{
-    console.log(user[index]);
-    index++;
-}while(index<user.length)
+do {
+  console.log(user[index]);
+  index++;
+} while (index < user.length);
+
+//FOR LOOP
+
+// for(let x = 1;x<=10;x++){
+//     console.log("Value of i is:" +i);
+// }
+let a = 1;//Initialization
+const names = ['taran','charan','kujan','deepakshi','saneha']
+for (let a = 0;a < user.length;a++){
+    console.log(user[a]);
+}
+
+//Break and Continue
